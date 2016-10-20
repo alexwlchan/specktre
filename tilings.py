b6d2dcf34946b2bf17ba0f11356cb531ed97fb1a
Alex Chan <alex@alexwlchan.net>
2016-10-20 12:49:45 +0100

    Update the code from writing the blog post
    Having to write a blog post about this code flushed out quite a few
    rough edges.  In particular:
    
    * Split out the work of generating unit shapes and scaling to
      arbitrary sizes.
    * Remove some hacky code from the hexagon generator.
    * Improved docstrings and comments.
    
    Plus some examples of the output!

--- tilings.py
+++ tilings.py
@@ -2,7 +2,12 @@
 # -*- encoding: utf-8 -*-
 """
 Generate regular tilings of the plane with PIL.
+
+Diagrams and background to this code are on my blog:
+http://alexwlchan.net/2016/10/tiling-the-plane-with-pillow/
 """
+
+from __future__ import division
 
 import math
 
@@ -12,8 +17,16 @@ CANVAS_WIDTH  = 400
 CANVAS_HEIGHT = 400
 
 
-def generate_squares(canvas_width, canvas_height, sq_side=25):
-    """Generate coordinates for a tiling of squares."""
+def _scale_coordinates(generator, image_width, image_height, side_length=50):
+    scaled_width = int(image_width / side_length)
+    scaled_height = int(image_height / side_length)
+
+    for coords in generator(scaled_width, scaled_height):
+        yield [(x * side_length, y * side_length) for (x, y) in coords]
+
+
+def generate_unit_squares(image_width, image_height):
+    """Generate coordinates for a tiling of unit squares."""
     # Iterate over the required rows and cells.  The for loops (x, y)
     # give the coordinates of the top left-hand corner of each square:
     #
@@ -23,81 +36,69 @@ def generate_squares(canvas_width, canvas_height, sq_side=25):
     #             |     |
     #  (x, y + 1) +-----+ (x + 1, y + 1)
     #
-    # Then we extend by 1 in each direction, and scale up for the length
-    # of the side of the squares.
-    #
-    for x in range(0, int(canvas_width / sq_side) + 1):
-        for y in range(0, int(canvas_height / sq_side) + 1):
-            coords = [(x, y), (x + 1, y), (x + 1, y + 1), (x, y + 1)]
-            yield [(a * sq_side, b * sq_side) for a, b in coords]
+    for x in range(image_width):
+        for y in range(image_height):
+            yield [(x, y), (x + 1, y), (x + 1, y + 1), (x, y + 1)]
 
 
-def generate_triangles(canvas_width, canvas_height, tr_side=25):
-    """Generate coordinates for a regular tiling of triangles."""
-    # Our triangles lie with one side paralle to the x-axis.  Let s be
+def generate_squares(*args, **kwargs):
+    """Generate coordinates for a tiling of squares."""
+    return _scale_coordinates(generate_unit_squares, *args, **kwargs)
+
+
+def generate_unit_triangles(image_width, image_height):
+    """Generate coordinates for a tiling of unit triangles."""
+    # Our triangles lie with one side parallel to the x-axis.  Let s be
     # the length of one side, and h the height of the triangle.
     #
     # The for loops (x, y) gives the coordinates of the top left-hand corner
     # of a pair of triangles:
     #
-    #           (x, y) +-----+ (x + s, y)
+    #           (x, y) +-----+ (x + 1, y)
     #                   \   / \
     #                    \ /   \
-    #    (x + s/2, y + h) +-----+ (x + 3s/2, y + h)
+    #    (x + 1/2, y + h) +-----+ (x + 3/2, y + h)
+    #
+    # where h = sin(60°) is the height of an equilateral triangle with
+    # side length 1.
     #
     # On odd-numbered rows, we translate by (s/2, 0) to make the triangles
     # line up with the even-numbered rows.
     #
     # To avoid blank spaces on the edge of the canvas, the first pair of
-    # triangles on each row starts at (-s, 0) -- one width before the edge
+    # triangles on each row starts at (-1, 0) -- one width before the edge
     # of the canvas.
+    h = math.sin(math.pi / 3)
 
-    # Width of the triangle is just the length of one side
-    tr_width = tr_side
+    for x in range(-1, image_width):
+        for y in range(int(image_height / h) + 1):
 
-    # Assume that one side of the triangle is parallel to the x-axis;
-    # then we have sin(60°) = tr_height / tr_side
-    tr_height = tr_side * math.sin(math.pi / 3)
+            # Add a horizontal offset on odd numbered rows
+            x_ = x if (y % 2 == 0) else x + 0.5
 
-    # Now we have a per-row height, we can iterate over the required rows.
-    for x in range(-1, int(canvas_width / tr_width) + 1):
-        for y in range(0, int(canvas_height / tr_height) + 1):
+            yield [(x_, y * h), (x_+1, y * h), (x_+0.5, (y+1) * h)]
+            yield [(x_+1, y * h), (x_+1.5, (y+1) * h), (x_+0.5, (y+1) * h)]
 
-            # Add the half-distance offset on odd-numbered rows.  For some
-            # reason, just adding directly to x screws up the other rows,
-            # so I'm using x_ as a stand-in for x.
-            if y % 2 == 0:
-                x_ = x
-            else:
-                x_ = x + 0.5
-
-            coords = [
-                # \/-shaped triangle
-                [(x_, y), (x_ + 1, y), (x_ + 0.5, y + 1)],
-
-                # /\-shaped triangle
-                [(x_ + 1, y), (x_ + 1.5, y + 1), (x_ + 0.5, y + 1)],
-            ]
-
-            for coord in coords:
-                yield [(a * tr_width, b * tr_height) for (a, b) in coord]
+def generate_triangles(*args, **kwargs):
+    """Generate coordinates for a tiling of triangles."""
+    return _scale_coordinates(generate_unit_triangles, *args, **kwargs)
 
 
-def generate_hexagons(canvas_width, canvas_height, hx_side=25):
-    """Generate coordinates for a regular tiling of hexagons."""
+def generate_unit_hexagons(image_width, image_height):
+    """Generate coordinates for a regular tiling of unit hexagons."""
     # Let s be the length of one side of the hexagon, and h the height
     # of the entire hexagon if one side lies parallel to the x-axis.
     #
     # The for loops (x, y) give the coordinate of one coordinate of the
     # hexagon, and the remaining coordinates fall out as follows:
     #
-    #                     (x, y) +-----+ (x + s, y)
+    #                     (x, y) +-----+ (x + 1, y)
     #                           /       \
     #                          /         \
-    #    (x - s/2, y + h / 2) +           + (x + 3s/2, y + h/2)
+    #         (x - 1/2 y + h) +           + (x + 3/2, y + h)
     #                          \         /
     #                           \       /
-    #                 (x, y + h) +-----+ (x + s, y + h)
+    #                 (x, y + 2h) +-----+ (x + 1, y + 2h)
     #
     # In each row we generate hexagons in the following pattern
     #
@@ -113,32 +114,35 @@ def generate_hexagons(canvas_width, canvas_height, hx_side=25):
     #
     # There are offsets to ensure we fill the entire canvas.
 
-    # The height of the entire hexagon satisfies
-    #    sin(60°) = 0.5 * hx_height / hx_side
-    hx_height = 2 * hx_side * math.sin(math.pi / 3)
+    # Half the height of the hexagon
+    h = math.sin(math.pi / 3)
 
-    for x in range(-1, int(canvas_width / hx_side) + 1, 3):
-        for y in range(0, int(canvas_height / hx_height) * 2 + 1):
+    for x in range(-1, image_width, 3):
+        for y in range(int(image_height / h) + 1):
 
-            # Add the half-distance offset on odd-numbered rows.
-            if y % 2 == 0:
-                x_ = x
-            else:
-                x_ = x + 1.5
+            # Add the horizontal offset on every other row
+            x_ = x if (y % 2 == 0) else x + 1.5
 
-            # Each row should only be offset vertically by half the height
-            # of the hexagon.
-            y_offst = y * hx_height * 0.5
-
-            coords = [
-                (x_, y), (x_ + 1, y), (x_ + 1.5, y + 0.5),
-                (x_ + 1, y + 1), (x_, y + 1), (x_ - 0.5, y + 0.5)
+            yield [
+                (x_,        y * h),
+                (x_ + 1,    y * h),
+                (x_ + 1.5, (y + 1) * h),
+                (x_ + 1,   (y + 2) * h),
+                (x_,       (y + 2) * h),
+                (x_ - 0.5, (y + 1) * h),
             ]
-            yield [(a * hx_side, b * hx_height - y_offst) for (a, b) in coords]
 
+
+def generate_hexagons(*args, **kwargs):
+    """Generate coordinates for a tiling of hexagons."""
+    return _scale_coordinates(generate_unit_hexagons, *args, **kwargs)
 
 
 def draw_tiling(coord_generator, filename):
+    """
+    Given a coordinate generator and a filename, render those coordinates
+    in a new image and save them to the file.
+    """
     im = Image.new('L', size=(CANVAS_WIDTH, CANVAS_HEIGHT))
     for shape in coord_generator(CANVAS_WIDTH, CANVAS_HEIGHT):
         ImageDraw.Draw(im).polygon(shape, outline='white')
@@ -149,3 +153,4 @@ if __name__ == '__main__':
     draw_tiling(generate_squares,   filename='squares.png')
     draw_tiling(generate_triangles, filename='triangles.png')
     draw_tiling(generate_hexagons,  filename='hexagons.png')
+    # draw_tiling(generate_unit_hexagons,  filename='uhexagons.png')
