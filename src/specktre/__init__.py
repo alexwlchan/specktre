855209c517d3a6f8f8b0b7a888acf9ac350eb821
Alex Chan <alex@alexwlchan.net>
2016-12-01 21:11:47 +0000

    Make the code a bit more usable as a library

--- src/specktre/__init__.py
+++ src/specktre/__init__.py
@@ -11,6 +11,26 @@ from .utils import new_filename
 __version__ = '0.3.0'
 
 
+def create_wallpaper(generator, color1, color2, width, height, filename):
+    """Create a wallpaper and saves it to a file.
+
+    :param generator: A function that takes two arguments `width` and `height`,
+        and generates `(x, y)` coordinates for a tiling of the plane.
+    :param color1: Start of the color range (`RGBColor` instance).
+    :param color2: End of the color range (`RGBColor` instance).
+    :param width: Width of the canvas (pixels).
+    :param height: Height of the canvas (pixels).
+    :param filename: Filename to save the image to.
+
+    """
+    coords = generator(width, height)
+    colors = random_color(color1, color2)
+    im = Image.new(mode='RGB', size=(width, height))
+    for coord, color in zip(coords, colors):
+        ImageDraw.Draw(im).polygon(coord, fill=color)
+    im.save(filename)
+
+
 def main():
     args = parse_args(version=__version__)
 
@@ -19,21 +39,19 @@ def main():
         'squares': generate_squares,
         'triangles': generate_triangles,
     }
-
-    # Get the coordinates of the tiling
     generator = generator_names[args.shape]
-    coords = generator(args.width, args.height)
-
-    # And the random colors
-    colors = random_color(args.color1, args.color2)
-
-    im = Image.new(mode='RGB', size=(args.width, args.height))
-    for coord, color in zip(coords, colors):
-        ImageDraw.Draw(im).polygon(coord, fill=color)
 
     if args.filename:
         filename = args.filename
     else:
         filename = new_filename()
-    im.save(filename)
+
+    create_wallpaper(
+        generator=generator_names[args.shape],
+        color1=args.color1,
+        color2=args.color2,
+        width=args.width,
+        height=args.height,
+        filename=filename)
+
     print('Saved new wallpaper as %s' % filename)
