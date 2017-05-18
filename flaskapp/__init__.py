c26dc95cbd83fdb4e1ee39168d5e155f6d5b24a3
Alex Chan <alex@alexwlchan.net>
2017-05-18 21:51:02 +0000

    Get SECRET_KEY from the environment

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
 app.config['WTF_CSRF_ENABLED'] = True
-app.config['SECRET_KEY'] = 'Nothing to do with the secret organisation'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Nothing to do with the secret organisation')
 
 from flaskapp import views
