b02f473cf6e364ab177c291492c696351ef565d6
Alex Chan <alex@alexwlchan.net>
2019-04-06 17:43:26 +0100

    Argument-parsing is really a CLI concern

--- src/specktre/cli.py
+++ src/specktre/cli.py
@@ -1,9 +1,40 @@
 # -*- encoding: utf-8 -*-
-"""Utilities for parsing input from the command-line."""
+"""Generate checkerboard wallpaper images.
+
+Usage:
+  specktre.py new --size=<size> --start=<start> --end=<end> [--squares | --triangles | --hexagons] [--name=<name>]
+  specktre.py -h
+
+Options:
+  -h --help          Show this screen.
+  --size=<size>      Size in pixels - WxH (e.g. 100x200)
+  --start=<start>    Start of the color range (hex, e.g. #01ab23)
+  --end=<end>        End of the color range (hex, e.g. #01ab23)
+  --squares          Tile with squares.
+  --triangles        Tile with triangles.
+  --hexagons         Tile with hexagons.
+  --name=<name>      (Optional) Name of the file to save to.
+
+"""  # noqa
 
 import re
+import sys
+
+import attr
+import docopt
 
 from .colors import RGBColor
+from .tilings import generate_hexagons, generate_squares, generate_triangles
+
+
+@attr.s
+class Settings(object):
+    generator = attr.ib()
+    width = attr.ib()
+    height = attr.ib()
+    start_color = attr.ib()
+    end_color = attr.ib()
+    name = attr.ib()
 
 
 def check_positive_integer(name, value):
@@ -49,3 +80,38 @@ def check_color_input(value):
     green = int(value[2:4], base=16)
     blue = int(value[4:6], base=16)
     return RGBColor(red, green, blue)
+
+
+def parse_args():
+    args = docopt.docopt(__doc__)
+
+    if args['--squares']:
+        generator = generate_squares
+    elif args['--triangles']:
+        generator = generate_triangles
+    elif args['--hexagons']:
+        generator = generate_hexagons
+    else:
+        generator = generate_squares
+
+    try:
+        width, height = args['--size'].split('x')
+    except ValueError:
+        sys.exit('--size should be in the form WxH; got %s' % args['--size'])
+
+    width = check_positive_integer(name='Width', value=width)
+    height = check_positive_integer(name='Height', value=height)
+
+    start_color = check_color_input(args['--start'])
+    end_color = check_color_input(args['--end'])
+
+    name = args['--name']
+
+    return Settings(
+        generator=generator,
+        width=width,
+        height=height,
+        start_color=start_color,
+        end_color=end_color,
+        name=name,
+    )
