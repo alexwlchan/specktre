026885994b3a3261ab2b3d218572309033d54164
Alex Chan <alex@alexwlchan.net>
2019-04-07 09:17:35 +0100

    Add tests for some more code in specktre.py

--- tests/test_specktre.py
+++ tests/test_specktre.py
@@ -1,9 +1,13 @@
 # -*- encoding: utf-8
 
+import os
+
+from PIL import Image
+
 from specktre.cli import Settings
 from specktre.colors import RGBColor
-from specktre.specktre import draw_speckled_wallpaper
-from specktre.tilings import generate_triangles
+from specktre.specktre import draw_speckled_wallpaper, save_speckled_wallpaper
+from specktre.tilings import generate_hexagons, generate_triangles
 
 
 def test_draw_speckled_wallpaper():
@@ -17,4 +21,41 @@ def test_draw_speckled_wallpaper():
     )
 
     im = draw_speckled_wallpaper(s)
+    assert im.size == (200, 300)
+
+
+def test_save_speckled_wallpaper(tmpdir):
+    name = tmpdir / "output.jpg"
+    s = Settings(
+        generator=generate_hexagons,
+        width=200,
+        height=300,
+        start_color=RGBColor(0, 0, 256),
+        end_color=RGBColor(0, 0, 0),
+        name=str(name)
+    )
+
+    save_speckled_wallpaper(s)
+    assert name.exists()
+    im = Image.open(name)
+    assert im.size == (200, 300)
+
+
+def test_save_speckled_wallpaper_generates_name(tmpdir):
+    os.chdir(str(tmpdir))
+    s = Settings(
+        generator=generate_hexagons,
+        width=200,
+        height=300,
+        start_color=RGBColor(0, 0, 256),
+        end_color=RGBColor(0, 0, 0),
+        name=None
+    )
+
+    assert tmpdir.listdir() == []
+    save_speckled_wallpaper(s)
+
+    assert len(tmpdir.listdir()) == 1
+    name = tmpdir.listdir()[0]
+    im = Image.open(name)
     assert im.size == (200, 300)
